575c61473586bc3ef9db6a046102004e54a54b17
G-Works <fabian.graf@live.de>
2026-05-05 10:05:45 +0200

    add header + contact section

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
   padding: "10px",
   fontWeight: "bold"
 }}>
-  Diese Seite befindet sich aktuell im Aufbau – Inhalte und Produkte werden laufend erweitert.
+  Diese Seite befindet sich aktuell im Aufbau – Inhalte, Produkte und Fotos werden laufend erweitert.
 </div>
 
       {/* HEADER */}
