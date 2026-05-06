222460e8df63b5d1b718ce3da467d3aa91dcee00
G-Works <fabian.graf@live.de>
2026-05-06 21:36:24 +0200

    update images

--- app/page.tsx
+++ app/page.tsx
@@ -472,7 +472,7 @@ export default function Home() {
       }}
     >
       <img
-        src="/images/windrad-engineering-v2.jpg"
+        src="/images/windrad-engineering-v3.jpg"
         alt="G-Works Engineering Projekte"
         style={{
           width: "100%",
