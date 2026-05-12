2bfc358e5513f19b3b06782bb5f4f4d67cd1d76e
G-Works <fabian.graf@live.de>
2026-05-12 16:50:05 +0200

    update

--- app/products/servo-cover/page.tsx
+++ app/products/servo-cover/page.tsx
@@ -19,17 +19,7 @@ export default function ServoCoverPage() {
           alignItems: "center",
         }}
       >
-        </div>
-        <img
-  src="/images/servo-cover-inst.jpg"
-  alt="Servo Cover Installation"
-  style={{
-    width: "100%",
-    borderRadius: "18px",
-    marginTop: "24px",
-    border: "1px solid #2a2a2a",
-  }}
-/>
+        
         <div>
           <img
             src="/images/servo-cover-v4.jpg"
