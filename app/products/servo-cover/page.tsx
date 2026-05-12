47d3cc63aa57ba111764eeaf2d933f07c209bf94
G-Works <fabian.graf@live.de>
2026-05-12 02:27:52 +0200

    update

--- app/products/servo-cover/page.tsx
+++ app/products/servo-cover/page.tsx
@@ -21,7 +21,7 @@ export default function ServoCoverPage() {
       >
         <div>
           <img
-            src="/images/servo-cover.jpg"
+            src="/images/servo-cover-v3.jpg"
             alt="Servo Cover"
             style={{
               width: "100%",
