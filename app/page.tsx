b8a2dcb7f862bc67d9b01f14f681cccc2e0ab47c
G-Works <fabian.graf@live.de>
2026-05-12 04:10:07 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ const products = [
   {
     title: "Haubensicherung hinten und vorne",
     text: "Leichte Zusatzsicherung zum Aufkleben.",
-    image: "/images/haubenbefestigung-hinten-und-vorne-v3.jpg",
+    image: "/images/akuplatte.jpg",
     link: "/products/haubenbefestigung",
   },
 
